6cd1aa650dc9b451f14c9c2bdcb0a214dd99a6f0
Brett Eiffert <eiffertbc@ornl.gov>
2024-02-22 15:43:10 -0500

    refactor, add timing

--- scheduler.py
+++ scheduler.py
@@ -29,26 +29,21 @@ def lines_to_lists(filename):
         job_list.append([q, int(0), int(0)])
     return job_list
 
-def schedule():
-    jobs = lines_to_lists(sys.argv[1])
-
-    metadata = jobs.pop(0)
-    num_jobs = int(metadata[0][0][0])
-    num_machines =  int(metadata[0][0][1])
-
-    machines = init_machine_list(num_machines)
-
+def schedule(jobs, machines, num_jobs, num_machines):    
     counter = 0
     while counter < num_jobs:
         for j in range(0, num_jobs):
+            print(len(jobs[j][0]))
             if (jobs[j][1] < len(jobs[j][0])):
                 cur_opp = jobs[j][1]
                 machine_id = jobs[j][0][cur_opp][0]
                 machine_time = jobs[j][0][cur_opp][1]
                 machines[machine_id][0].append((j, machine_time))
             else:
+                print(counter)
                 counter = counter + 1
         for m in range(0, num_machines):
+            #print(m)
             if machines[m][0]:
                 sorted_machine_ops = sorted(machines[m][0], key=itemgetter(1))
                 machines[m][0] = sorted_machine_ops
@@ -68,14 +63,28 @@ def schedule():
                         machines[m][1] = comp_job_time
                         result.append([job_id, opp_id, machines[m][1]])
 
-    print(machines)
+    #print(machines)
     #print(len(result))
     return result
 
+print("========================")
+
+start_file = time.time()
+
+jobs = lines_to_lists(sys.argv[1])
+metadata = jobs.pop(0)
+
+num_jobs = int(metadata[0][0][0])
+num_machines =  int(metadata[0][0][1])
+machines = init_machine_list(num_machines)
+
+end_file = time.time()
+print(sys.argv[1])
+print("Load file time: " + str(end_file-start_file))
+
 start = time.time()
-print(schedule())
+schedule(jobs, machines, num_jobs, num_machines)
 end = time.time()
+print("Schedule time: " + str(end-start))
 
-print(end-start)
-
-
+print("========================")
